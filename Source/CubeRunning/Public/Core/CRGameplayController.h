6bd5c810f50a59693b9a38dcf8abc4f849c50f95
hunter200262@mail.ru <hunter200262@mail.ru>
2022-04-16 16:15:26 +0600

    [Escape menu] -Add Escape menu -Add Escape menu call logic and paus is controller

---
+++ Source/CubeRunning/Public/Core/CRGameplayController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "CRGameplayController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CUBERUNNING_API ACRGameplayController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
